b8a10aacc7cd895b97211db9123b92406069dcda
oluwarotimitimileyin <135017393+oluwarotimitimileyin@users.noreply.github.com>
2026-03-12 00:17:31 +0200

    Add Admin Dashboard component

---
+++ src/pages/admin/index.js
@@ -0,0 +1,15 @@
+export default function Admin(){
+
+  return(
+
+    <div style={{padding:"40px"}}>
+
+      <h1>Admin Dashboard</h1>
+
+      <p>Manage drivers and rides here.</p>
+
+    </div>
+
+  )
+
+}
